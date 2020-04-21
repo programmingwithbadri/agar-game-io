9d4acf20314ae34df352cb49f218082bbbd6e914
Badri Narayanan <programmingwithbadri@outlook.com>
2020-04-21 17:08:43 +0530

    Use the mousemove event to update the player using the mouse position(bug)

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -3,8 +3,8 @@ function init() {
 }
 
 // Canvas Drawing
-let randomX = Math.floor(500 * Math.random() + 10);
-let randomY = Math.floor(500 * Math.random() + 10);
+player.locX = Math.floor(500 * Math.random() + 10);
+player.locY = Math.floor(500 * Math.random() + 10);
 function draw() {
   // Context was set as 2D in the uiStuff.js
   context.beginPath();
@@ -15,7 +15,7 @@ function draw() {
   // Arg 3 - Radius of the circle
   // Arg 4 - Start of the Radian. For eg) 0 - 3 o clock position
   // Arg 5 - stop of radian. Here its 3
-  context.arc(randomX, randomY, 10, 0, Math.PI * 2);
+  context.arc(player.locX , player.locY, 10, 0, Math.PI * 2);
   context.fill();
 
   // Will add border around the circle
@@ -23,7 +23,56 @@ function draw() {
   context.strokeStyle = "rgb(0,255,0)";
   context.stroke();
 
-  // Recursively calls the draw method to update the canvas 
+  // Recursively calls the draw method to update the canvas
   // based on x and y axis that will be provided by mouse move in the UI
   requestAnimationFrame(draw);
 }
+
+canvas.addEventListener("mousemove", (event) => {
+  // Get the mouse position in UI
+  const mousePosition = {
+    x: event.clientX,
+    y: event.clientY,
+  };
+
+  // Returns the angle between the mouse position
+  const angleDeg =
+    (Math.atan2(
+      mousePosition.y - canvas.height / 2,
+      mousePosition.x - canvas.width / 2
+    ) *
+      180) /
+    Math.PI;
+
+  // Get the quadrant of the mouse (1,2,3,4)
+  if (angleDeg >= 0 && angleDeg < 90) {
+    xVector = 1 - angleDeg / 90;
+    yVector = -(angleDeg / 90);
+  } else if (angleDeg >= 90 && angleDeg <= 180) {
+    xVector = -(angleDeg - 90) / 90;
+    yVector = -(1 - (angleDeg - 90) / 90);
+  } else if (angleDeg >= -180 && angleDeg < -90) {
+    xVector = (angleDeg + 90) / 90;
+    yVector = 1 + (angleDeg + 90) / 90;
+  } else if (angleDeg < 0 && angleDeg >= -90) {
+    xVector = (angleDeg + 90) / 90;
+    yVector = 1 - (angleDeg + 90) / 90;
+  }
+
+  speed = 10;
+  xV = xVector;
+  yV = yVector;
+
+  // Move the player location by the mouse position
+  if (
+    (player.locX < 5 && player.xVector < 0) ||
+    (player.locX > 500 && xV > 0)
+  ) {
+    player.locY -= speed * yV;
+  } else if ((player.locY < 5 && yV > 0) || (player.locY > 500 && yV < 0)) {
+    player.locX += speed * xV;
+  } else {
+    player.locX += speed * xV;
+    player.locY -= speed * yV;
+  }
+});
