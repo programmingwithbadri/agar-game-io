2988095edafaac5c463b327c023829ed58fe22ae
Badri Narayanan <programmingwithbadri@outlook.com>
2020-04-21 16:20:56 +0530

    Folder structure

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const socketIo = require("socket.io");
+const helmet = require("helmet");
 
 const app = express();
+app.use(helmet())
 
 app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen("9000");
 
 const io = socketIo(expressServer);
+
+module.exports = {
+    app,
+    io
+}
