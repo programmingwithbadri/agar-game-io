37b38895a22b1ef6471db6390f9f57b4e241514b
Badri Narayanan <programmingwithbadri@outlook.com>
2020-04-21 16:38:11 +0530

    Getting the DOM setup

---
+++ public/uiStuff.js
@@ -0,0 +1,35 @@
+// Get window width and height
+let windowHeight = $(window).height();
+let windowWidth = $(window).width();
+
+let player =  {}
+
+// Get the canvas to show in the window
+let canvas = document.querySelector("#the-canvas");
+let context = canvas.getContext("2d");
+canvas.width = windowWidth;
+canvas.height = windowHeight;
+
+// Show the login modal when the page is loaded
+$(window).load(()=>{
+    $('#loginModal').modal('show')
+})
+
+// Submit event
+$('.name-form').submit((event)=>{
+    event.preventDefault()
+    player.name = document.querySelector('#name-input').value;
+
+    // Once user name added hide the login modal
+    $('#loginModal').modal('hide');
+
+    // Show the spawn modal 
+    $('#spawnModal').modal('show')
+    document.querySelector('.player-name').innerHTML = player.name
+})
+
+// Once game starts hide the modal
+$('.start-game').click((event)=>{
+    $('.modal').modal('hide');
+    $('.hiddenOnStart').removeAttr('hidden');
+})
