e79940c674ee2cac2b20c12f82e09725d9b2f80a
Badri Narayanan <programmingwithbadri@outlook.com>
2020-04-21 20:32:39 +0530

    Create the orbs in the server

---
+++ sockets/classes/Orb.js
@@ -0,0 +1,19 @@
+class Orb {
+  constructor() {
+    this.color = getRandomColor();
+    this.locX = Math.floor(500 * Math.random());
+    this.locY = Math.floor(500 * Math.random());
+    this.radius = 5;
+  }
+
+  getRandomColor() {
+    // gets random rgb
+    const r = Math.floor(Math.random() * 200 + 50);
+    const g = Math.floor(Math.random() * 200 + 50);
+    const b = Math.floor(Math.random() * 200 + 50);
+
+    return `rgb(${r}, ${g},${b})`;
+  }
+}
+
+module.exports = Orb;
--- sockets/index.js
+++ sockets/index.js
@@ -1,1 +1,15 @@
-const io = require('../server').io;
+const io = require("../server").io;
+const Orb = require("./classes/Orb");
+
+let orbs = [];
+
+initGame();
+
+// Runs at the beginning of each game
+function initGame() {
+  for (let i = 0; i < 500; i++) {
+    orbs.push(new Orb());
+  }
+}
+
+module.exports = io;
