9c97a38e187a0f815b8c292a286ce1cbe162020a
Badri Narayanan <programmingwithbadri@outlook.com>
2020-04-21 21:07:13 +0530

    Minor UI fix

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -8,13 +8,13 @@ player.locY = Math.floor(500 * Math.random() + 10);
 
 // Context was set as 2D in the uiStuff.js
 function draw() {
+  // Reset the canvas translate
+  context.setTransform(1, 0, 0, 1, 0, 0);
+
   // Clear the canvas whenever the draw method is called
   // So that we wont see the player circle is dragging across the window
   // when the mouse is moved
   context.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Reset the canvas translate
-  context.setTransform(1, 0, 0, 1, 0, 0);
 
   // Move the canvas but set the focus to the player position
   const camX = -player.locX + canvas.width / 2;
@@ -26,12 +26,6 @@ function draw() {
   context.beginPath();
   context.fillStyle = "rgb(255,0,0)";
 
-  // Arc will create the circle/semi
-  // Arg1, 2 - X,Y of the center of arc
-  // Arg 3 - Radius of the circle
-  // Arg 4 - Start of the Radian. For eg) 0 - 3 o clock position
-  // Arg 5 - stop of radian. Here its 3
-  context.arc(player.locX, player.locY, 10, 0, Math.PI * 2);
   context.fill();
 
   // Will add border around the circle
